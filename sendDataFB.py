0994efcccbe50525afd17aeb2f8be2e6ecd82cd8
mukaddim98 <mukaddim1998@gmail.com>
2020-12-21 07:40:56 +0000

    difference calculation

--- sendDataFB.py
+++ sendDataFB.py
@@ -1,25 +1,61 @@
 from firebase import firebase
 from datetime import datetime
+from gpiozero import DigitalInputDevice
 import time
+import datetime
+
+print("Program started!")
 
 FBConn = firebase.FirebaseApplication('https://water-me-433c0-default-rtdb.firebaseio.com/',None)
+d0_input = DigitalInputDevice(21)
+
+#reset
+url = '/MyTestData/'
+result = FBConn.get(url,None)
+if(result != None):
+    Entries = list(result.keys())
+    for entry in Entries:
+        FBConn.delete(url,entry)
+
+#first entry
+soilMoisture = d0_input.value
+dateTime = datetime.datetime.now()
+data_to_upload = {
+    'Soil Moisture' : soilMoisture,
+    'Time' : dateTime
+}
+result = FBConn.post(url, data_to_upload)
+print(result)
 
 while True:
-    soilMoisture = 12;
-    dateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-
-    data_to_upload = {
-        'Soil Moisture' : soilMoisture,
-        'Time' : dateTime
-    }
-    
-    url = '/MyTestData/'
-    result = FBConn.post(url, data_to_upload)
-
-    print(result)
-
     time.sleep(2)
 
-    result = FBConn.get('/MyTestData/', None)
-    lastEntry = list(result.keys())[0]
-    FBConn.delete('/MyTestData/', lastEntry)
+    oldSoilMoisture = soilMoisture
+    soilMoisture = d0_input.value
+    changeMoisture = soilMoisture - oldSoilMoisture 
+
+    if(changeMoisture != 0):
+        oldDateTime = dateTime
+        dateTime = datetime.datetime.now()
+        changeDateTime = dateTime - oldDateTime
+
+        result = FBConn.get(url,None)
+        lastEntry = list(result.keys())[0]
+        FBConn.delete(url,lastEntry)
+
+        if(soilMoisture):
+            print('Moisture threshold reached!!!')
+        else:
+            print('You need to water your plant')
+
+        data_to_upload = {
+            'Soil Moisture' : soilMoisture,
+            'Time' : dateTime
+        }
+    
+        result = FBConn.post(url, data_to_upload)
+        
+        print("Time Difference:",changeDateTime)
+        print(result)
+
+
